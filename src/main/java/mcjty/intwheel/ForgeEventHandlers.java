4360785ed6e657a29f26fdeef84a94b8674f1740
McJty <mcjty1@gmail.com>
2017-06-12 05:09:10 +0200

    Reenabled event handler

--- src/main/java/mcjty/intwheel/ForgeEventHandlers.java
+++ src/main/java/mcjty/intwheel/ForgeEventHandlers.java
@@ -3,6 +3,7 @@ package mcjty.intwheel;
 import mcjty.intwheel.playerdata.PlayerWheelConfiguration;
 import mcjty.intwheel.playerdata.PlayerProperties;
 import mcjty.intwheel.playerdata.PropertiesDispatcher;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
@@ -10,15 +11,15 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ForgeEventHandlers {
-//@todo
-//    @SubscribeEvent
-//    public void onEntityConstructing(AttachCapabilitiesEvent.Entity event){
-//        if (event.getEntity() instanceof EntityPlayer) {
-//            if (!event.getEntity().hasCapability(PlayerProperties.PLAYER_WHEEL_CONFIGURATION, null)) {
-//                event.addCapability(new ResourceLocation(InteractionWheel.MODID, "hotkeys"), new PropertiesDispatcher());
-//            }
-//        }
-//    }
+
+    @SubscribeEvent
+    public void onEntityConstructing(AttachCapabilitiesEvent<Entity> event){
+        if (event.getObject() instanceof EntityPlayer) {
+            if (!event.getObject().hasCapability(PlayerProperties.PLAYER_WHEEL_CONFIGURATION, null)) {
+                event.addCapability(new ResourceLocation(InteractionWheel.MODID, "hotkeys"), new PropertiesDispatcher());
+            }
+        }
+    }
 
     @SubscribeEvent
     public void onPlayerCloned(PlayerEvent.Clone event) {
