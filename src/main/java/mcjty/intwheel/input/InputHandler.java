e2506d4cb7d8e6eb7617268fe3bc06fd64f73d29
McJty <mcjty1@gmail.com>
2023-01-29 08:33:06 +0100

    Use 'h' key

--- src/main/java/mcjty/intwheel/input/InputHandler.java
+++ src/main/java/mcjty/intwheel/input/InputHandler.java
@@ -3,10 +3,6 @@ package mcjty.intwheel.input;
 
 import mcjty.intwheel.gui.GuiWheel;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.BlockHitResult;
-import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -24,17 +20,7 @@ public class InputHandler {
 
     private void checkWheelKey() {
         if (KeyBindings.keyOpenWheel.consumeClick()) {
-            HitResult mouseOver = Minecraft.getInstance().hitResult;
-            if (mouseOver instanceof BlockHitResult blockHitResult) {
-                BlockPos pos = blockHitResult.getBlockPos();
-                // @todo 1.19.2
-                Minecraft.getInstance().setScreen(new GuiWheel());
-//                player.openGui(InteractionWheel.instance, GuiProxy.GUI_WHEEL, player.getEntityWorld(), pos.getX(), pos.getY(), pos.getZ());
-            } else {
-                // @todo 1.19.2
-                Minecraft.getInstance().setScreen(new GuiWheel());
-//                player.openGui(InteractionWheel.instance, GuiProxy.GUI_WHEEL, player.getEntityWorld(), player.getPosition().getX(), player.getPosition().getY(), player.getPosition().getZ());
-            }
+            Minecraft.getInstance().setScreen(new GuiWheel());
         }
     }
 
