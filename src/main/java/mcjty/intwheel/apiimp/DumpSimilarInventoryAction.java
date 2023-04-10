4d4d745ee7ad6aed9d9fba4ef347697a03a16518
McJty <mcjty1@gmail.com>
2023-04-10 07:49:28 +0200

    Removed usage of deprecated capability instances

--- src/main/java/mcjty/intwheel/apiimp/DumpSimilarInventoryAction.java
+++ src/main/java/mcjty/intwheel/apiimp/DumpSimilarInventoryAction.java
@@ -8,7 +8,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemHandlerHelper;
 
@@ -57,8 +57,8 @@ public class DumpSimilarInventoryAction implements IWheelAction {
     @Override
     public void performServer(Player player, Level world, BlockPos pos, boolean extended) {
         BlockEntity te = world.getBlockEntity(pos);
-        if (te != null && te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).isPresent()) {
-            te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(inventory -> {
+        if (te != null && te.getCapability(ForgeCapabilities.ITEM_HANDLER, null).isPresent()) {
+            te.getCapability(ForgeCapabilities.ITEM_HANDLER, null).ifPresent(inventory -> {
                 for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
                     ItemStack stack = player.getInventory().getItem(i);
                     if (!stack.isEmpty() && isSimilar(inventory, stack)) {
--- src/main/java/mcjty/intwheel/apiimp/ExtractWheelAction.java
+++ src/main/java/mcjty/intwheel/apiimp/ExtractWheelAction.java
@@ -8,7 +8,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 
 public class ExtractWheelAction implements IWheelAction {
 
@@ -30,8 +30,8 @@ public class ExtractWheelAction implements IWheelAction {
     @Override
     public void performServer(Player player, Level world, BlockPos pos, boolean extended) {
         BlockEntity te = world.getBlockEntity(pos);
-        if (te != null && te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).isPresent()) {
-            te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(inventory -> {
+        if (te != null && te.getCapability(ForgeCapabilities.ITEM_HANDLER, null).isPresent()) {
+            te.getCapability(ForgeCapabilities.ITEM_HANDLER, null).ifPresent(inventory -> {
                 for (int i = 0; i < inventory.getSlots(); i++) {
                     ItemStack stack = inventory.getStackInSlot(i);
                     if (!stack.isEmpty()) {
